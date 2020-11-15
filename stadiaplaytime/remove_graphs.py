6a4769e01c75d1ce01d8771a8d8580e7e83c78e4
kirktrekkie <kirkx10@gmail.com>
2020-11-15 19:24:03 +0000

    Added script that removes old graphs

---
+++ stadiaplaytime/remove_graphs.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+PATH = 'static'
+
+
+def remove_old_graps(time_limit=1700):
+    files = [f for f in os.listdir(PATH) if ".png" in f]
+    current_time = time.time()
+
+    for file in files:
+        mod_time = os.path.getmtime(os.path.join(PATH, file))
+        if current_time - mod_time > time_limit:
+            os.remove(os.path.join(PATH, file))
+
+
+if __name__ == '__main__':
+    remove_old_graps()
